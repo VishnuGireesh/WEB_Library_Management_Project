3299fcad03c763c57a817d515135742cbe293576
Vishnu Gireesh <vishnugireesh310630@gmail.com>
2019-10-24 22:16:49 +0530

    student login code

---
+++ studlogin.php
@@ -0,0 +1,65 @@
+<?php
+
+include "connection.php";
+
+?>
+
+<html>
+    <head>
+        <title>student login</title>
+        <link rel="stylesheet"  href="studlogin.css">
+    </head>
+    <body>
+        <div id="container">
+            <div class="row">
+                <div class="column">
+
+                </div>
+                <div class="column2">
+                    <div id="loginbox">
+                    <form  method="post">
+                        <div class="imgcontainer">
+                            <img src="images/img_avatar2.png" alt="Avatar" class="avatar">
+                        </div>
+
+                        <div class="container">
+                            <label for="uname"><b>Username</b></label>
+                            <input type="text" name="un" placeholder="Enter Username" required>
+
+                            <label for="psw"><b>Password</b></label>
+                            <input type="password" name="pass" placeholder="Enter Password" required>
+                                
+                            <button type="submit" name="sub">Login</button>
+                            
+                        </div>
+
+                    </form>
+
+                    <?php
+           
+           if(isset($_POST["sub"]))
+                {
+                    $count=0;
+                    $res=mysqli_query($link,"select * from studreg where uname='$_POST[un]' && pass='$_POST[pass]'");
+                    $count=mysqli_num_rows($res);
+
+                    if($count==0)
+                    {
+                        ?>
+                       
+                       <div class="alert">
+                             <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+                             <strong>OOPS!</strong> Wrong Username or Password
+                        </div>
+
+
+                    <?php
+                    }
+                }
+                    ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
